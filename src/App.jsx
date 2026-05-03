ffd551acfce3b3f370f2b97bea7474014be27250
Raginaakhter <raginaakhter052@gmail.com>
2026-05-03 14:36:01 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+   <div>
+    <h1 className='text-3xl font-bold text-red-500'>
+      lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate!
+    </h1>
+   </div>
+  )
+}
+
+export default App
