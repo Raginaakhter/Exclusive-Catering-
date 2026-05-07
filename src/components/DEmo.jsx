15c0a5419dcd3000bdb035e65fbc259aca8cd4bc
Raginaakhter <raginaakhter052@gmail.com>
2026-05-07 11:11:48 +0600

    added demo

---
+++ src/components/DEmo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const DEmo = () => {
+    return (
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa ducimus odio
+             cupiditate dolor fugit hic delectus enim? Necessitatibus nulla, dolorem neque aperiam magnam ad accusantium numquam soluta eveniet ipsum tempora?
+        </div>
+    );
+};
+
+export default DEmo;
