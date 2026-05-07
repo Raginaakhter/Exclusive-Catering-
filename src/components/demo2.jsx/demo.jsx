135d2075256e8854513cddaa671796ed58cef1e2
Raginaakhter <raginaakhter052@gmail.com>
2026-05-07 11:29:34 +0600

    first commit

---
+++ src/components/demo2.jsx/demo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const demo = () => {
+    return (
+        <div>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus sit, magnam aut voluptas explicabo quis dolore itaque quasi, eveniet ipsum nesciunt porro! Tempora odit quibusdam eaque voluptas delectus et cupiditate.
+        </div>
+    );
+};
+
+export default demo;
