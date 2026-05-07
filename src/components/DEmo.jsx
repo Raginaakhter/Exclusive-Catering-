565828d5cf99284d06f3165ab7167b87be6dac50
Raginaakhter <raginaakhter052@gmail.com>
2026-05-07 11:18:21 +0600

    added

--- src/components/DEmo.jsx
+++ src/components/DEmo.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const DEmo = () => {
     return (
         <div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa ducimus odio
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa ducimus odio.jdd
              cupiditate dolor fugit hic delectus enim? Necessitatibus nulla, dolorem neque aperiam magnam ad accusantium numquam soluta eveniet ipsum tempora?
         </div>
     );
